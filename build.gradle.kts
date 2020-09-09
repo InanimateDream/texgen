bf2236c6ca2fbde36d6d9e9a35bfa7cccb2f2438
ice1000 <ice1000kotlin@foxmail.com>
2020-09-10 01:43:02 +0800

    [ fmt ] Create .editorconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,29 +2,30 @@ group = "org.ice1000.texgen"
 version = "0.1"
 
 plugins {
-    java
-    `java-library`
-    `maven-publish`
+  java
+  `java-library`
+  `maven-publish`
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testCompile("junit", "junit", "4.12")
+  api("org.jetbrains", "annotations", "19.0.0")
+  testImplementation("junit", "junit", "4.12")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-    withSourcesJar()
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+  withSourcesJar()
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "6.5"
+  gradleVersion = "6.5"
 }
 
 tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
+  options.encoding = "UTF-8"
 }
