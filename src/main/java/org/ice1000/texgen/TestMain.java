3e13120dac1300168734566b2094f4b7bb624da0
ice1000 <ice1000kotlin@foxmail.com>
2020-09-10 03:05:14 +0800

    [ gen ] Misc

--- src/main/java/org/ice1000/texgen/TestMain.java
+++ src/main/java/org/ice1000/texgen/TestMain.java
@@ -2,9 +2,21 @@ package org.ice1000.texgen;
 
 import org.jetbrains.annotations.TestOnly;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
 @TestOnly
 public class TestMain {
-  public static void main(String... args) {
+  public static void main(String... args) throws IOException {
+    var testTokens = List.of(
+      Token.from("line1", Attribute.DEFAULT),
+      Token.from("\n", Attribute.DEFAULT),
+      Token.from("line2", Attribute.DEFAULT)
+    );
+    var gen = new Generator(Paths.get("build"), new GenConfig());
+    gen.gen("test.tex", testTokens.stream());
+    gen.gen("test2.tex", testTokens.stream());
     System.out.println(114514);
   }
 }
