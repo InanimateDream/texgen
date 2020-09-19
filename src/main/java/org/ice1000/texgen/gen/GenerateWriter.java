a35aa559963842083e06712a1b645cbb8fc30ddf
ice1000 <ice1000kotlin@foxmail.com>
2020-09-20 04:54:24 +0800

    [ writer ] Functional programming

--- src/main/java/org/ice1000/texgen/gen/GenerateWriter.java
+++ src/main/java/org/ice1000/texgen/gen/GenerateWriter.java
@@ -1,10 +1,12 @@
 package org.ice1000.texgen.gen;
 
 import org.ice1000.texgen.code.Token;
+import org.ice1000.texgen.tex.TeXComponent;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -18,11 +20,10 @@ public class GenerateWriter {
     @NotNull Stream<@NotNull Token> tokens,
     @NotNull GenConfig config
   ) throws IOException {
-    var res = tokens.reduce(""
-      , (acc, tk) ->
-        acc + config.teXConfig.basicConvertor.apply(tk).toCode().toString()
-      , String::concat
-    );
+    var res = tokens
+      .map(config.teXConfig.basicConvertor)
+      .map(TeXComponent::toCode)
+      .collect(Collectors.joining(" "));
     writer.write(res);
   }
 }
